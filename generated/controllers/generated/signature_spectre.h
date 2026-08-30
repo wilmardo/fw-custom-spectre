ede46850dd12f4e04bfe3af8274b726210d127af
GitHub git update Action <action@github.com>
2026-08-30 15:45:33 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_spectre.h
+++ generated/controllers/generated/signature_spectre.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3373878132
-#define TS_SIGNATURE "rusEFI main.2026.08.30.spectre.3373878132"
+#define SIGNATURE_HASH 4276859796
+#define TS_SIGNATURE "rusEFI main.2026.08.30.spectre.4276859796"
